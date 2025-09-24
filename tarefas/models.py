bcd83c261c9b9dd8b0ff6729d1d7c5d076ab28e9
Italo <ibm676869@gmail.com>
2025-09-24 17:00:16 +0000

    feature: adicionara as Tarefas e suas propiedades de prioridade e conclusao

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    class Prioridade(models.TextChoices):
+        PRIORIDADE_BAIXA = 'B', 'Baixa'
+        PRIORIDADE_MEDIA = 'M', 'Média'
+        PRIORIDADE_ALTA = 'A', 'Alta'
+
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True, null=True)
+    concluida = models.BooleanField(default=False)
+    prioridade = models.CharField(
+        max_length=1,
+        choices=Prioridade.choices,
+        default=Prioridade.PRIORIDADE_MEDIA,
+    )
+
+    def __str__(self):
+        return self.titulo

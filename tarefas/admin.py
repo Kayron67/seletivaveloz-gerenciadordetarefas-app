8a67fcebea38a5a974aab8824f3a7ea197de2f81
Italo <ibm676869@gmail.com>
2025-09-24 17:39:18 +0000

    feat: users se tornara Usuarios

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Tarefa
+
+User._meta.verbose_name = 'Usuário'
+User._meta.verbose_name_plural = 'Usuários'
 
 admin.site.register(Tarefa)

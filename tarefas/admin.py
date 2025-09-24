7e6d6f9fe19be0ddc3adb5b97c8b1ff8d31035f2
Italo <ibm676869@gmail.com>
2025-09-24 19:09:11 +0000

    feat: intregara o django rest framework ao projeto

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import Tarefa
 
+# Customiza o adimin para exibir o nome User em português 
+## Por ser modelo padrão não alterei ele, apenas a troca de nome por consistencia
+###  
 User._meta.verbose_name = 'Usuário'
 User._meta.verbose_name_plural = 'Usuários'
 

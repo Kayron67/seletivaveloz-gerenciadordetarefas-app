eb34d93f312788836cd61e82155f02f590336bc2
Italo <ibm676869@gmail.com>
2025-09-25 20:23:40 +0000

    chore: modificara e centralizara as urls em apenas um ponto

--- gerenciador_de_tarefas_app/urls.py
+++ gerenciador_de_tarefas_app/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import DefaultRouter
+from tarefas import views as tarefas_views
+from projetos import views as projetos_views
+
+router = DefaultRouter()
+router.register(r'tarefas', tarefas_views.TarefaViewSet, basename='tarefa')
+router.register(r'usuarios', tarefas_views.UsuarioViewSet, basename='usuario')
+router.register(r'projetos', projetos_views.ProjetoViewSet, basename='projeto')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tarefas.urls')),
-    path('api/', include('projetos.urls')),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),  
 ]
--- projetos/urls.py
+++ projetos/urls.py
@@ -1,10 +1,1 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
-
-router = DefaultRouter()
-router.register(r'projetos', views.ProjetoViewSet, basename='projeto')
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = []

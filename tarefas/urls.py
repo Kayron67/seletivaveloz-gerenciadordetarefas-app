33af16d339213f8d8d1ee46d3e39da391955871e
Italo <ibm676869@gmail.com>
2025-09-25 20:23:40 +0000

    chore: modificara e centralizara as urls em apenas um ponto

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,11 +1,1 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
-
-router = DefaultRouter()
-router.register(r'tarefas', views.TarefaViewSet, basename='tarefa')
-router.register(r'usuarios', views.UsuarioViewSet, basename='usuario')
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = []

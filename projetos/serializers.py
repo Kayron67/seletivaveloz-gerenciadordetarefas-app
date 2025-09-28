090dbea2636709f4a7beedd671872b1c332dd6ee
Italo <ibm676869@gmail.com>
2025-09-28 13:19:29 +0000

    feat: adiciona aos projetos link criar novas tarefas

--- projetos/serializers.py
+++ projetos/serializers.py
@@ -5,7 +5,12 @@ from tarefas.serializers import TarefaResumidaEmProjetoSerializer
 class ProjetoSerializer(serializers.HyperlinkedModelSerializer):
     tarefas = TarefaResumidaEmProjetoSerializer(many=True, read_only=True)
 
+    adicionar_tarefa_url = serializers.HyperlinkedIdentityField(
+        view_name='projeto-tarefas-list',
+        lookup_url_kwarg='projeto_pk'
+    )
+
     class Meta:
         model = Projeto
-        fields = ['url', 'id', 'titulo', 'descricao', 'criador', 'membros', "tarefas"]
+        fields = ['url', 'id', 'titulo', 'descricao', 'criador', 'membros', 'adicionar_tarefa_url', 'tarefas']
         read_only_fields = ['criador']

af049a8e2c4a343d95186f3ef671943449d80eaf
Italo <ibm676869@gmail.com>
2025-09-28 18:52:08 +0000

    fix: altera admin para evitar  criar tarefas sem projetos

--- projetos/admin.py
+++ projetos/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Projeto
+from tarefas.models import Tarefa
 
-admin.site.register(Projeto)
+class TarefaInlineParaProjetos(admin.TabularInline):
+    model = Tarefa
+    fields = ('titulo',)
+    extra = 1
+    show_change_link = True
+
+class ProjetoAdmin(admin.ModelAdmin):
+    inlines = [TarefaInlineParaProjetos]
+    list_display = ('titulo', 'criador')
+    search_fields = ('titulo', 'descricao')
+
+admin.site.register(Projeto, ProjetoAdmin)
--- tarefas/admin.py
+++ tarefas/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from .models import Tarefa
 
 class TarefaAdmin(admin.ModelAdmin):
-    readonly_fields = ('projeto',)
     list_display = ('titulo', 'projeto', 'concluida', 'prioridade')
     list_filter = ('projeto', 'concluida', 'prioridade')
+    search_fields = ('titulo', 'descricao')
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('projeto',)
+        return ()
+
+    def has_add_permission(self, request):
+        return False
 
 admin.site.register(Tarefa, TarefaAdmin)

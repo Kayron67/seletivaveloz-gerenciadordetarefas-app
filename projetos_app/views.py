e3c9495a77c6f49c63f2fbe9059cdaa735c10e33
Italo <ibm676869@gmail.com>
2025-09-29 19:25:25 +0000

    chore: usuario pode pesquisar seus projetos e os que e membro

--- projetos_app/views.py
+++ projetos_app/views.py
@@ -2,11 +2,15 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Projeto
 from .serializers import ProjetoSerializer
+from django.db.models import Q
 
 class ProjetoViewSet(viewsets.ModelViewSet):
-    queryset = Projeto.objects.all()
     serializer_class = ProjetoSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Projeto.objects.filter(Q(criador=user) | Q(membros=user)).distinct()
 
     def perform_create(self, serializer):
         serializer.save(criador=self.request.user)
